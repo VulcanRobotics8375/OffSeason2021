0ae3a7fcaee60211b1c5f511b9f75385f67366d3
Mathew Zheng <mathewzheng9999@gmail.com>
2021-07-04 22:56:52 -0700

    Some Basic Drivetrain Stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
@@ -6,12 +6,36 @@ import org.firstinspires.ftc.teamcode.robotcorelib.util.Subsystem;
 
 public class Drivetrain extends Subsystem {
 
+    private DcMotor fl, fr, bl, br;
+
+
     @Override
     public void init() {
-        //hardware map goes here
+        fl = hardwareMap.dcMotor.get("front_left");
+        fr = hardwareMap.dcMotor.get("front_right");
+        bl = hardwareMap.dcMotor.get("back_left");
+        br = hardwareMap.dcMotor.get("back_right");
 
+        setDrivetrainMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        setDrivetrainMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
+        fl.setDirection(DcMotorSimple.Direction.REVERSE);
+        fr.setDirection(DcMotorSimple.Direction.FORWARD);
+        bl.setDirection(DcMotorSimple.Direction.REVERSE);
+        br.setDirection(DcMotorSimple.Direction.FORWARD);
     }
 
-    //other methods go here
+    public void setPower(double fl, double fr, double bl, double br) {
+        this.fl.setPower(fl);
+        this.fr.setPower(fr);
+        this.bl.setPower(bl);
+        this.br.setPower(br);
+    }
+
 
 }
